48661d058bcbbec57d858cfa2f23a2ba36c7b39b
Nobuyuki Aoki <aokinobu@gmail.com>
2015-11-03 22:10:41 +0100

    fix test

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+export FILENAME=client-0.0.1-SNAPSHOT.jar
+export CLIENT_JAR=../../glytoucanClient/target/$FILENAME
 export API_KEY=asdf
 export API_CONTRIBUTOR_ID=254;
-java -jar ../../glytoucanClient/target/client-0.0.1-SNAPSHOT.jar -cp ./target/sync-0.0.1-SNAPSHOT.jar org.glytoucan.sync.SyncApp
+cp $CLIENT_JAR target/
+cd target;jar xvf $FILENAME;cd ..
+java -cp ./target/$FILENAME:./target/sync-0.0.1-SNAPSHOT.jar:./target/lib/* org.glytoucan.sync.SyncApp
