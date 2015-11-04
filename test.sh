dad33e066fa026dbe8d67dede130b2fe368d5e48
Nobuyuki Aoki <aokinobu@gmail.com>
2015-11-04 10:55:16 +0100

    relative path using ~

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export FILENAME=client-0.0.1-SNAPSHOT.jar
-export CLIENT_JAR=../../glytoucanClient/target/$FILENAME
+export CLIENT_JAR=~/glytoucanClient/target/$FILENAME
 export API_KEY=asdf
 export API_CONTRIBUTOR_ID=254;
 cp $CLIENT_JAR target/
