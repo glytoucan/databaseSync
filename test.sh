d55fe578ba77f69b1a794547cd9babac0aed5b32
Nobuyuki Aoki <aokinobu@gmail.com>
2015-11-05 22:55:05 +0100

    fix to be absolute path for client.jar

--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 export FILENAME=client-0.0.1-SNAPSHOT.jar
-export CLIENT_JAR=~/glytoucanClient/target/$FILENAME
-export API_KEY=asdf
+export CLIENT_JAR=/home/aoki/workspace/glytoucanClient/target/$FILENAME
+export API_KEY=JDUkMjAxNTEwMjkwMTQyMjQkYU8wYWcucWF5VXU5eW9CUVlmT3l0YjZ2ejRza2o3eWRDcGJDNVFtMThKMA==
 export API_CONTRIBUTOR_ID=254;
+echo cp $CLIENT_JAR target/
 cp $CLIENT_JAR target/
 cd target;jar xvf $FILENAME;cd ..
 java -cp ./target/$FILENAME:./target/sync-0.0.1-SNAPSHOT.jar:./target/lib/* org.glytoucan.sync.SyncApp
