4833c0b6672ab6fdb8a9a28228438637e82e5140
Nobuyuki Aoki <aokinobu@gmail.com>
2015-11-05 23:05:13 +0100

    oops

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export FILENAME=client-0.0.1-SNAPSHOT.jar
 export CLIENT_JAR=/home/aoki/workspace/glytoucanClient/target/$FILENAME
-export API_KEY=JDUkMjAxNTEwMjkwMTQyMjQkYU8wYWcucWF5VXU5eW9CUVlmT3l0YjZ2ejRza2o3eWRDcGJDNVFtMThKMA==
+export API_KEY=asdf
 export API_CONTRIBUTOR_ID=254;
 echo cp $CLIENT_JAR target/
 cp $CLIENT_JAR target/
