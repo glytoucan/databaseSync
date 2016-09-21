0f30e6c450ee89c2bd1a63253f39108274dbdbc5
aoki <aokinobu@gmail.com>
2016-09-21 18:40:39 +0900

    sample scripts

---
+++ simpleEnv.groovy
@@ -0,0 +1,8 @@
+@GrabConfig(autoDownload=false)
+@GrabResolver(name='glycoinfo', root='http://nexus.glycoinfo.org/content/groups/public/')
+@GrabResolver(name='glycoinfo-snapshots', root='http://nexus.glycoinfo.org/content/repositories/snapshots/')
+@Grab('org.glytoucan:client:1.2.5-TOCO-SNAPSHOT')
+import org.glytoucan.client.AntiSpringJavaRest
+def asjr = new AntiSpringJavaRest()
+String result = asjr.register('Gal')
+println result;
