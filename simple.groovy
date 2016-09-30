341e9092b2e42a16c3851c6eb3dced64cd434ed7
aoki <aokinobu@gmail.com>
2016-09-30 19:26:04 +0900

    simple

--- simple.groovy
+++ simple.groovy
@@ -1,8 +1,12 @@
-@GrabConfig(autoDownload=false)
 @GrabResolver(name='glycoinfo', root='http://nexus.glycoinfo.org/content/groups/public/')
 @GrabResolver(name='glycoinfo-snapshots', root='http://nexus.glycoinfo.org/content/repositories/snapshots/')
 @Grab('org.glytoucan:client:1.2.5-TOCO-SNAPSHOT')
 import org.glytoucan.client.AntiSpringJavaRest
 def asjr = new AntiSpringJavaRest()
-String result = asjr.register('Gal')
+
+String sequence = "Gal"
+String contributorId = "254"
+String apiKey = "abcdefghijk"
+
+String result = asjr.register(sequence, contributorId, apiKey)
 println result;
